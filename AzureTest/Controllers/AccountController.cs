6765305d51a70b2369ed87942260002677b56fab
Артищев Дмитрий Сергеевич <artischev@mkb.ru>
2017-04-13 14:59:01 +0300

    отключил редирект при авторизации

--- AzureTest/Controllers/AccountController.cs
+++ AzureTest/Controllers/AccountController.cs
@@ -70,7 +70,7 @@ namespace AzureTest.Controllers
                 if (result.Succeeded)
                 {
                     _logger.LogInformation(1, "User logged in.");
-                    return RedirectToLocal(returnUrl);
+                    //return RedirectToLocal(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
                 {
